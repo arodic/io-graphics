01a757935db4d9c47d9d05794920ab5d061180a8
Aki Rodic <aleksandar.xyz@gmail.com>
2020-05-31 19:42:43 -0700

    Updated package.json.

--- rollup.config.js
+++ rollup.config.js
@@ -40,8 +40,8 @@ function svg() {
 
 const externals = [
 	path.resolve('../three.js/build/three.module.js'),
-	path.resolve('build/iogui.js'),
-	path.resolve('build/io-elements.js')
+	path.resolve('../iogui/build/iogui.js'),
+	path.resolve('../iogui/build/io-elements.js')
 ];
 
 function makeTarget(src, target) {
